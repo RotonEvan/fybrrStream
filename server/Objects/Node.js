5fa5c86bdaee61cea0fe2172325ee9c55d3040c2
prashantbodh099 <prashantbodh099@gmail.con>
2020-12-18 19:46:24 +0530

    Room.js created

--- server/Objects/Node.js
+++ server/Objects/Node.js
@@ -1,11 +1,12 @@
 module.exports = class Node {
-    constructor (node_id, score, limit, adj_list = [], parent = null) {
+    constructor (node_id, score, limit, websocket) {
         this.node_id = node_id;
         this.score = score;
         this.limit = limit;
         this.slots = limit;
-        this.adj_list = adj_list;
-        this.parent = parent;
+        this.adj_list = [];
+        this.parent = null;
+        this.websocket = websocket;
     }
 
     //getters
@@ -34,6 +35,10 @@ module.exports = class Node {
         return this.parent;
     }
 
+    getWebsocket(){
+        return this.websocket;
+    }
+
     // setters and functions
 
     decreaseSlots () {
@@ -44,16 +49,30 @@ module.exports = class Node {
         this.adj_list.push(node);
     }
 
-    minScoreChild () {
-        minimum = this.adj_list[0];
-        min_limit = node_data[adjacency_matrix[0][0]].limit;
-        for (i in adjacency_matrix[0]){
-            if (node_data[adjacency_matrix[0][i]].limit < min_limit) {
-                min_limit = node_data[adjacency_matrix[0][i]].limit;
-                minimum = adjacency_matrix[0][i];
+    minLimitChild () {
+        var minimum = this.adj_list[0];
+        var min_limit = minimum.getLimit();
+        
+        this.adj_list.forEach(i => {
+            if (i.getLimit() < min_limit) {
+                min_limit = i.getLimit();
+                minimum = i;
             }    
-        }
+        });
         return minimum; 
+    }
+
+    maxScoreChild () {
+        var maximum = this.adj_list[0];
+        var max_score = maximum.getScore();
+        
+        this.adj_list.forEach(i => {
+            if (i.getScore() > max_score) {
+                max_score = i.getScore();
+                maximum = i;
+            }    
+        });
+        return maximum;
     }
 
     setParent (node) {
--- server/Objects/Room.js
+++ server/Objects/Room.js
@@ -1,6 +1,51 @@
+const Node = require("./Node");
+
 module.exports = class Room {
-    constructor (room_id, node_data = null) {
+    constructor (room_id) {
         this.room_id = room_id;
-        this.node_data = node_data;
+        this.node_data = {};
+        this.size = 1;
     }
+
+    getRoomID () {
+        return this.room_id;
+    }
+
+    getSize () {
+        return this.size;
+    }
+    
+    addNode (id, score, limit, websocket) {
+        this.node_data[id] = new Node(id, score, limit, websocket);
+        this.size++;
+    }
+
+    getBestNodes (size = this.getSize()) {
+        var n = Math.log(size);
+        var best_nodes = [];
+
+        this.node_data.sort(function(x, y) {
+            if (x.getScore() < y.getScore()) {
+              return -1;
+            }
+            if (x.getScore() > y.getScore()) {
+              return 1;
+            }
+            return 0;
+          });
+        
+        this.node_data.forEach(i => {
+            if (i.getSlots() > 0){
+                var node_dic = {'id' : i.getID(), 'score' : i.getScore(), 'slots' : i.getSlots()};
+                best_nodes.push(node_dic);
+                if (best_nodes.length == n){
+                    break;
+                }
+            }    
+        });
+
+        return best_nodes;
+    }
+    
+
 }
