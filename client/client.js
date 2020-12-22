c1dc66e1af1054419eb1722bb6ee62028c1af395
RotonEvan <debajyotih@iitbhilai.ac.in>
2020-12-22 17:17:16 +0530

    errata

--- client/client.js
+++ client/client.js
@@ -33,17 +33,17 @@ var peerConnectionConfig = {
         { 'urls': 'stun:stun.l.google.com:19302' },
         { 'urls': 'stun:stun1.l.google.com:19302' },
         { 'urls': 'stun:stun2.l.google.com:19302' },
-        { 'urls': 'stun:stun3.l.google.com:19302' },
-        { 'urls': 'stun:stun4.l.google.com:19302' },
-        { 'urls': 'stun:stun.ekiga.net' },
-        { 'urls': 'stun:stun.ideasip.com' },
-        { 'urls': 'stun:stun.rixtelecom.se' },
-        { 'urls': 'stun:stun.schlund.de' },
+        // { 'urls': 'stun:stun3.l.google.com:19302' },
+        // { 'urls': 'stun:stun4.l.google.com:19302' },
+        // { 'urls': 'stun:stun.ekiga.net' },
+        // { 'urls': 'stun:stun.ideasip.com' },
+        // { 'urls': 'stun:stun.rixtelecom.se' },
+        // { 'urls': 'stun:stun.schlund.de' },
         { 'urls': 'stun:stun.stunprotocol.org:3478' },
-        { 'urls': 'stun:stun.voiparound.com' },
-        { 'urls': 'stun:stun.voipbuster.com' },
-        { 'urls': 'stun:stun.voipstunt.com' },
-        { 'urls': 'stun:stun.voxgratia.org' }
+        // { 'urls': 'stun:stun.voiparound.com' },
+        // { 'urls': 'stun:stun.voipbuster.com' },
+        // { 'urls': 'stun:stun.voipstunt.com' },
+        // { 'urls': 'stun:stun.voxgratia.org' }
     ]
 };
 
@@ -171,7 +171,7 @@ function setUpPeer(peer, initCall = false) {
         localStream.getTracks().forEach(t => {
             peerConnections[peer].pc.addTrack(t, localStream);
         });
-        peerConnections[peer].pc.addStream(localStream);
+        // peerConnections[peer].pc.addStream(localStream);
     }
   
     if (initCall) {
